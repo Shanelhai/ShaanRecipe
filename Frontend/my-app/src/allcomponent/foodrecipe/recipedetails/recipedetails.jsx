afa23715d78f473f59345db009f6851f5452b092
Shanelhai <shanelhai7@email.com>
2026-04-27 15:41:11 +0530

    updata

--- Frontend/my-app/src/allcomponent/foodrecipe/recipedetails/recipedetails.jsx
+++ Frontend/my-app/src/allcomponent/foodrecipe/recipedetails/recipedetails.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
-import "./recipeDetails.css";
+import "./recipedetails.css";
 import Footer from "../../footer/footer";
 
 
